ab647bfd45c2d42a7691d93196593731faa763da
Caio Victor <64919516+caiovictorR@users.noreply.github.com>
2020-05-18 14:56:50 -0300

    Add files via upload

---
+++ kiloMetro.java
@@ -0,0 +1,35 @@
+package kiloMetro
+        
+
+import java.util.Scanner;
+
+public class kiloMetro {
+    
+    public static void main (String[] args)
+    //definição de variaveis
+            
+     double km,consumo,preco,resp;
+     
+     //entrada
+     Scanner entrada = new Scanner (system.in);
+     
+     System.out.println("Quantos km ira percorrer:");
+     km =entrada.next.Double();
+     
+     System.out.println("Quanto seu carro consome de gasolina:");
+    consumo =entrada.next.Double();
+    
+    //processamento
+    resp=(km/consumo)*preco;
+            
+    //saida
+   System.out.println("Você gastará em média:$"+resp);
+    
+    
+    
+     
+             
+      
+            
+    
+}
