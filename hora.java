76e787a1a05384e1db91afc7d36eeab6f67d38b1
Caio Victor <64919516+caiovictorR@users.noreply.github.com>
2020-05-19 17:35:30 -0300

    Add files via upload

---
+++ hora.java
@@ -0,0 +1,31 @@
+package hora
+
+import java.util.Scanner;
+
+public class hora {
+     public  static  void  main ( String [] args ) {
+        // definição de variáveis
+        double mes,hora,resp;
+        
+        Scanner entrada = new Scanner(system.in);
+        
+        //entrada
+        System.out.println("quanto trabalhou este mes (em decimal):")
+         mes = entrada.nextDouble;
+                 
+        System.out.println("quanto ganho por hora:")
+         hora = entrada.nextDouble;
+         
+         //processamento
+         resp=(mes*hora)*1.70
+                 
+        //saida
+        System.out.println("voce ira receber:"+resp)
+                 
+                 
+                 
+      
+        
+    
+    
+}
